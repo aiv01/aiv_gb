3a71b87f976dd279c254a729afa7c6cf92b5ba8c
Roberto De Ioris <roberto.deioris@gmail.com>
2018-12-13 11:17:28 +0100

    implemented opcode 3

--- cpu/opcodes_00.c
+++ cpu/opcodes_00.c
@@ -14,8 +14,16 @@ static int aiv_gb_opcode_01(aiv_gameboy *gb)
     return 12;
 }
 
+// LD (BC), A
+static int aiv_gb_opcode_02(aiv_gameboy *gb)
+{
+    aiv_gb_memory_write8(gb, gb->bc, gb->a);
+    return 8;
+}
+
 void aiv_gb_register_opcodes_00(aiv_gameboy *gb)
 {
     gb->opcodes[0x00] = aiv_gb_opcode_00;
     gb->opcodes[0x01] = aiv_gb_opcode_01;
+    gb->opcodes[0x02] = aiv_gb_opcode_02;
 }
