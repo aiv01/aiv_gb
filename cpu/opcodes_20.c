a28a413e73fef0d62aca6233c5761fa37d99e2a1
aannun <alex.aaav@hotmail.it>
2018-12-13 12:49:10 +0100

    updates

--- cpu/opcodes_20.c
+++ cpu/opcodes_20.c
@@ -11,9 +11,17 @@ static int aiv_gb_opcode_20(aiv_gameboy *gb)
     return 8;
 }
 
-
+// LD HL, d16
+static int aiv_gb_opcode_21(aiv_gameboy *gb)
+{
+    gb->hl = aiv_gb_memory_read16(gb, gb->pc);
+    gb->pc += 2;
+    return 12;
+}
 
 void aiv_gb_register_opcodes_20(aiv_gameboy *gb)
 {
     gb->opcodes[0x20] = aiv_gb_opcode_20;
+    gb->opcodes[0x21] = aiv_gb_opcode_21;
+
 }
--- tests/test_opcodes_20.c
+++ tests/test_opcodes_20.c
@@ -29,9 +29,24 @@ TEST(not_jr_nz_r8)
     ASSERT_THAT(gb.ticks == 8)
 }
 
+TEST(ld_hl_d16)
+{
+    aiv_gameboy gb;
+    aiv_gb_init(&gb);
+
+    gb.cartridge[0] = 0x21;
+    gb.cartridge[1] = 0x01;
+    gb.cartridge[2] = 0x02;
+    aiv_gb_tick(&gb);
+    ASSERT_THAT(gb.hl == 0x0201);
+    ASSERT_THAT(gb.ticks == 12);
+    ASSERT_THAT(gb.pc == 3);
+}
+
 void aiv_gb_tests_run_opcodes_20()
 {
     RUN_TEST(jr_nz_r8);
     RUN_TEST(not_jr_nz_r8);
+    RUN_TEST(ld_hl_d16);
 
 }
