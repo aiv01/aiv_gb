ff90833188881df213d38e07786c7757b1e0162c
Simone <sem961@gmail.com>
2018-12-13 11:20:06 +0100

    Fix opcode0x30

--- cpu/opcodes_30.c
+++ cpu/opcodes_30.c
@@ -6,7 +6,7 @@
 //    return 4;
 //}
 
-// LD BC, d16
+// LD SP, d16
 static int aiv_gb_opcode_31(aiv_gameboy *gb)
 {
     gb->sp = aiv_gb_memory_read16(gb, gb->pc);
@@ -14,7 +14,7 @@ static int aiv_gb_opcode_31(aiv_gameboy *gb)
     return 12;
 }
 
-void aiv_gb_register_opcodes_00(aiv_gameboy *gb)
+void aiv_gb_register_opcodes_30(aiv_gameboy *gb)
 {
     //gb->opcodes[0x30] = aiv_gb_opcode_30;
     gb->opcodes[0x31] = aiv_gb_opcode_31;
