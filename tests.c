e16f57507f51928c15af700b2ffa7aee7414ed3b
EmilianoVella <emilianovella@yahoo.it>
2018-12-18 10:58:46 +0100

    Update tests.c

--- tests.c
+++ tests.c
@@ -4,25 +4,7 @@ int tests_succeded = 0;
 int tests_failed = 0;
 int tests_executed = 0;
 
-// void aiv_gb_tests_run_opcodes_00();
-// void aiv_gb_tests_run_opcodes_10();
-// void aiv_gb_tests_run_opcodes_20();
-// void aiv_gb_tests_run_opcodes_30();
-
-// void aiv_gb_tests_run_opcodes_40();
 void aiv_gb_tests_run_opcodes_50();
-// void aiv_gb_tests_run_opcodes_60();
-// void aiv_gb_tests_run_opcodes_70();
-
-// void aiv_gb_tests_run_opcodes_80();
-// void aiv_gb_tests_run_opcodes_90();
-// void aiv_gb_tests_run_opcodes_a0();
-// void aiv_gb_tests_run_opcodes_b0();
-
-// void aiv_gb_tests_run_opcodes_c0();
-// void aiv_gb_tests_run_opcodes_d0();
-// void aiv_gb_tests_run_opcodes_e0();
-// void aiv_gb_tests_run_opcodes_f0();
 
 int main(int argc, char **argv)
 {
