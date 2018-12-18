005e69efaead2465ac5b9313d3467ca5f6c76269
Roberto De Ioris <roberto.deioris@gmail.com>
2018-12-18 12:34:19 +0100

    implemented f000 memory

--- memory.c
+++ memory.c
@@ -58,6 +58,15 @@ static void aiv_gb_memory_write_external_ram(aiv_gameboy *gb, u16_t address, u8_
     gb->ram[address] = value;
 }
 
+static u8_t aiv_gb_memory_read_f000(aiv_gameboy *gb, u16_t address)
+{
+    return 0;
+}
+
+static void aiv_gb_memory_write_f000(aiv_gameboy *gb, u16_t address, u8_t value)
+{
+}
+
 void aiv_gb_memory_init(aiv_gameboy *gb)
 {
     gb->memory_read[0x0] = aiv_gb_memory_read_cartridge;
@@ -78,6 +87,8 @@ void aiv_gb_memory_init(aiv_gameboy *gb)
     gb->memory_read[0xc] = aiv_gb_memory_read_ram;
     gb->memory_read[0xd] = aiv_gb_memory_read_ram;
 
+    gb->memory_read[0xf] = aiv_gb_memory_read_f000;
+
     gb->memory_write[0x0] = aiv_gb_memory_write_cartridge;
     gb->memory_write[0x1] = aiv_gb_memory_write_cartridge;
     gb->memory_write[0x2] = aiv_gb_memory_write_cartridge;
@@ -95,6 +106,8 @@ void aiv_gb_memory_init(aiv_gameboy *gb)
 
     gb->memory_write[0xc] = aiv_gb_memory_write_ram;
     gb->memory_write[0xd] = aiv_gb_memory_write_ram;
+
+    gb->memory_write[0xf] = aiv_gb_memory_write_f000;
 }
 
 void aiv_gb_memory_write16(aiv_gameboy *gb, u16_t address, u16_t value)
