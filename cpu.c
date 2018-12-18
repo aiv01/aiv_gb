414a2c3caa533add035b30a8ef548f7108be2179
Roberto De Ioris <roberto.deioris@gmail.com>
2018-12-18 11:10:35 +0100

    added unregistered opcode

--- cpu.c
+++ cpu.c
@@ -1,5 +1,13 @@
 #include <aiv_gb.h>
 
+static int aiv_gb_unregistered_opcode(aiv_gameboy *gb)
+{
+    u8_t opcode = gb->memory_read[(gb->pc - 1) >> 12](gb, gb->pc - 1);
+    fprintf(stderr, "unregistered opcode: %02x\n", opcode);
+    exit(1);
+    return 0;
+}
+
 int aiv_gb_tick(aiv_gameboy *gb)
 {
     u8_t opcode = aiv_gb_memory_read8(gb, gb->pc++);
@@ -38,8 +46,13 @@ void aiv_gb_init(aiv_gameboy *gb)
 {
     memset(gb, 0, sizeof(aiv_gameboy));
 
-
     aiv_gb_memory_init(gb);
+
+    int i = 0;
+    for (i = 0; i < 256; i++)
+    {
+        gb->opcodes[i] = aiv_gb_unregistered_opcode;
+    }
 
     aiv_gb_register_opcodes_00(gb);
     aiv_gb_register_opcodes_10(gb);
